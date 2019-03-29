66d7ad1dc65b2f7f15bcb5d98b694ac24784fa01
Michael Yankura <myankura210@gmail.com>
2019-03-29 11:19:53 -0500

    initial commit

---
+++ src/lib/grunt/bgShell.js
@@ -0,0 +1,8 @@
+module.exports = {
+    launchAPI: {
+        cmd: "json-server -p 8000 -w ../../api/smallBusiness.json"
+    },
+    _defaults: {
+        bg: true
+    }
+  };
---
+++ src/scripts/main.js
@@ -0,0 +1,12 @@
+{/* <article class="employee">
+    <header class="employee__name">
+        <h1>Rainu Ittycheriah</h1>
+    </header>
+    <section class="employee__department">
+        Works in the IT department
+    </section>
+    <section class="employee__computer">
+        Currently using a 2015 MacBook Pro
+    </section>
+</article> */}
+
